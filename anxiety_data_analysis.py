6d2d84eed6bdab9b8339b9c5362fa18f13e7a134
Suelem Audelo <84360844+saudelo@users.noreply.github.com>
2026-06-17 09:15:45 -0700

    cleaning it up

--- anxiety_data_analysis.py
+++ anxiety_data_analysis.py
@@ -18,75 +18,11 @@ GRID    = "#E0E0E0"
 
 try:
    #load file
-    file_name = "ai_student_impact_dataset.csv"
+    file_name = "clean.csv"
     filepath = os.path.join(file_name)
-    df = pd.read_csv(filepath)
+    clean_df = pd.read_csv(filepath)
     print("File loaded successfully!")
        
-
-
-    #exploration
-    rows_1,columns_1= df.shape  
-
-    print(f"rows {rows_1}")  
-    print(f"columns {columns_1}")  
-
-    #clean_data
- 
-    #No rows with missing data
-    clean_df = df.dropna()
-    rows_2,columns_2= clean_df.shape  
-
-    print(f"rows {rows_2}")  
-    print(f"columns {columns_2}") 
-
-
-    
-    #All ids are unique
-    clean_df.drop_duplicates(subset='Student_ID')
-    rows_3,cols_3 = clean_df.shape
-    print(f"rows {rows_3}")  
-    print(f"columns {cols_3}") 
-
-    #MORE EXPLORATION
-
-    #In case it's relvant, data frames by Major
-    #Major: STEM
-    stem_mask = clean_df["Major_Category"].str.strip().str.upper() == "STEM"
-    stem_df= clean_df[stem_mask]
-    r,c = stem_df.shape
-    print(f"rows {r}")  
-    print(f"columns {c}") 
-
-    #Major: Humanities
-    stem_mask = clean_df["Major_Category"].str.strip() == "Humanities"
-    stem_df= clean_df[stem_mask]
-    r,c = stem_df.shape
-    print(f"rows {r}")  
-    print(f"columns {c}") 
-
-    #Major: Medical
-    med_mask = clean_df["Major_Category"].str.strip() == "Medical"
-    med_df= clean_df[med_mask]
-    r,c = med_df.shape
-    print(f"rows {r}")  
-    print(f"columns {c}") 
-
-    #Major: Arts
-    arts_mask = clean_df["Major_Category"].str.strip()== "Arts"
-    arts_df= clean_df[arts_mask]
-    r,c = arts_df.shape
-    print(f"rows {r}")  
-    print(f"columns {c}") 
-
-    #Major: Business
-    biz_mask = clean_df["Major_Category"].str.strip() == "Business" 
-    biz_df= clean_df[biz_mask]   
-    r,c = biz_df.shape  
-    print(f"rows {r}")  
-    print(f"columns {c}") 
-
-
 
     #Ai usage in correlation to self-reported anxiety
 
@@ -94,6 +30,10 @@ try:
 
     #Burnout risk    Burnout_Risk_Level Anxiety_Level_During_Exams  Post_Semester_GPA Institutional_Policy
     #Weekly_GenAI_Hours Traditional_Study_Hours Perceived_AI_Dependency
+    rows_1,columns_1= clean_df.shape  
+
+    print(f"rows {rows_1}")  
+    print(f"columns {columns_1}")  
 
     anx_df =clean_df[["Weekly_GenAI_Hours","Traditional_Study_Hours", "Perceived_AI_Dependency","Burnout_Risk_Level","Anxiety_Level_During_Exams","Institutional_Policy"]].copy()
     anx_df = anx_df.sort_values(by="Anxiety_Level_During_Exams", ascending=False) #More exploring
