fa84df8a811b9901b85477a7c7c5fc332834aaf6
luke_f <luke.fields@protonmail.com>
2026-06-16 17:29:09 -0700

    formatted views of data for skill retention

---
+++ clean_skill_retention.py
@@ -0,0 +1,75 @@
+import numpy as np
+import pandas as pd
+
+DATA_FILE = "ai_student_impact_dataset (1).csv"
+YEAR_OUTPUT = "skill_retention_by_year.csv"
+PRE_GPA_OUTPUT = "skill_retention_by_pre_gpa.csv"
+POST_GPA_OUTPUT = "skill_retention_by_post_gpa.csv"
+
+border_80 = "="*80+"\n"
+GPA_bins = [0, 1, 2, 3, 4]
+GPA_labels = ["0-1", "1-2", "2-3", "3-4"]
+# pre_GPA_labels = ["pre: 0-1", "pre: 1-2", "pre: 2-3", "pre: 3-4"]
+# post_GPA_labels = ["post: 0-1", "post: 1-2", "post: 2-3", "post: 3-4"]
+hour_bins = [0, 5, 10, 15, 20, 25, 30, 35, 40]
+hour_labels = ["0-5", "5-10", "10-15", "15-20", "20-25", "25-30", "30-35", "35-40"]
+
+df = pd.read_csv(DATA_FILE)
+
+df["Weekly_hours_bracket"] = pd.cut(df["Weekly_GenAI_Hours"], bins=hour_bins, labels=hour_labels)
+df["Pre_Semester_GPA_bracket"] = pd.cut(df["Pre_Semester_GPA"], bins=GPA_bins, labels=GPA_labels)
+df["Post_Semester_GPA_bracket"] = pd.cut(df["Post_Semester_GPA"], bins=GPA_bins, labels=GPA_labels)
+
+year_subset = df[["Year_of_Study", "Weekly_GenAI_Hours", "Skill_Retention_Score", "Weekly_hours_bracket"]]
+pre_GPA_subset = df[["Pre_Semester_GPA", "Pre_Semester_GPA_bracket", "Weekly_GenAI_Hours", "Skill_Retention_Score", "Weekly_hours_bracket"]]
+post_GPA_subset = df[["Post_Semester_GPA", "Post_Semester_GPA_bracket", "Weekly_GenAI_Hours", "Skill_Retention_Score", "Weekly_hours_bracket"]]
+
+print(df["Post_Semester_GPA"].max())
+print(df.info())
+print(year_subset.info())
+
+year_pivot = year_subset.pivot_table(
+    values="Skill_Retention_Score",
+    index="Weekly_hours_bracket",
+    columns="Year_of_Study",
+    aggfunc="mean",
+    fill_value=0,
+    margins=True,
+    margins_name="Average Total"
+)
+
+pre_GPA_pivot = pre_GPA_subset.pivot_table(
+    values="Skill_Retention_Score",
+    index="Weekly_hours_bracket",
+    columns="Pre_Semester_GPA_bracket",
+    aggfunc="mean",
+    fill_value=0,
+    margins=True,
+    margins_name="Average Total"
+)
+
+post_GPA_pivot = post_GPA_subset.pivot_table(
+    values="Skill_Retention_Score",
+    index="Weekly_hours_bracket",
+    columns="Post_Semester_GPA_bracket",
+    aggfunc="mean",
+    fill_value=0,
+    margins=True,
+    margins_name="Average Total"
+)
+
+year_order = ["Freshman", "Sophomore", "Junior", "Senior", "Graduate"]
+pre_GPA_order = ["pre: 0-1", "pre: 1-2", "pre: 2-3", "pre: 3-4",]
+post_GPA_order = ["post: 0-1", "post: 1-2", "post: 2-3", "post: 3-4"]
+
+year_pivot = year_pivot.reindex(year_order, axis=1, fill_value=0.0).round(2)
+pre_GPA_pivot = pre_GPA_pivot.reindex(GPA_labels, axis=1, fill_value=0.0).round(2)
+post_GPA_pivot = post_GPA_pivot.reindex(GPA_labels, axis=1, fill_value=0.0).round(2)
+
+print(year_pivot)
+print(pre_GPA_pivot)
+print(post_GPA_pivot)
+
+year_pivot.to_csv(YEAR_OUTPUT)
+pre_GPA_pivot.to_csv(PRE_GPA_OUTPUT)
+post_GPA_pivot.to_csv(POST_GPA_OUTPUT)
